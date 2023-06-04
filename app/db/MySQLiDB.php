d0a0f953b3a03fbf015fa8be015c1d88e0eb4333
Maxmilián Dao <maxmilian.dao@outlook.com>
2023-06-05 00:58:54 +0200

    Update MySQLiDB.php

--- app/db/MySQLiDB.php
+++ app/db/MySQLiDB.php
@@ -6,6 +6,7 @@ require_once "IDB.php";
 // Implement IDB Interface
 class MySQLiDB implements IDB {
     private $db;
+    
     function __construct() {
         $host = getenv('DB_HOST');
         $username = getenv('DB_UN');
@@ -18,37 +19,30 @@ class MySQLiDB implements IDB {
         }
 
         $this->db = $mysqli;
-        /*
-        $this->dbhost = $dbHost;
-        $this->dbuser = $dbUser;
-        $this->dbpass = $dbPass;
-        $this->dbname = $dbName;
-        //$this->dbport = "";
-        */
-        /*
-        $this->dbhost = "127.0.0.1";
-        $this->dbuser = "root";
-        $this->dbpass = "root";
-        $this->dbname = "kalani";
-        */
     }
+    
     // Select what u want from database
     public function _select(string $table, array $fields = [], array $conditions = []): array {
         $sql = "SELECT " . (!empty($fields) ? implode(",", $fields) : "*") . " FROM " . $table;
+        
         if (!empty($conditions)) {
             $where = " WHERE " . implode(" AND ", array_map(function($key, $value) {
                 return "$key='" . mysqli_real_escape_string($this->db, $value) . "'";
             }, array_keys($conditions), $conditions));
             $sql .= $where;
         }
+        
         $result = mysqli_query($this->db, $sql);
+        
         if (!$result) {
             return [];
         }
+        
         $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
         mysqli_free_result($result);
         return $rows;
     }
+    
     // Insert into database
     public function _insert(string $table, array $data): bool {
         $keys = array_keys($data);
@@ -58,6 +52,7 @@ class MySQLiDB implements IDB {
         $sql = "INSERT INTO $table (" . implode(',', $keys) . ") VALUES (" . implode(',', $values) . ")";
         return mysqli_query($this->db, $sql);
     }
+    
     // Update database
     public function _update(string $table, string $primaryKey, int $id, array $data): bool {
         $set = array_map(function ($key, $value) {
@@ -67,17 +62,21 @@ class MySQLiDB implements IDB {
         $sql = "UPDATE $table SET $set WHERE $primaryKey = $id";
         return mysqli_query($this->db, $sql);
     }
+    
     // Delete from database
     public function _delete(string $table, int $id, string $primary_key = 'id'): bool {
         $sql = "DELETE FROM $table WHERE $primary_key = $id";
         return mysqli_query($this->db, $sql);
     }
+    
     // Delete from database more things
     public function _delete_anime(string $table, array $ids, array $primary_keys = ['id']): bool {
         $where = [];
+       
         foreach ($primary_keys as $primary_key) {
             $where[] = "$primary_key = ?";
         }
+        
         $where_clause = implode(' AND ', $where);
         $sql = "DELETE FROM $table WHERE $where_clause";
         $stmt = mysqli_prepare($this->db, $sql);
@@ -88,6 +87,7 @@ class MySQLiDB implements IDB {
         $sql = "DELETE FROM $table WHERE joinDate <= DATE_SUB(CURRENT_DATE(), INTERVAL 3 MINUTE)";
         return mysqli_query($this->db, $sql);
     }
+    
     // Return error message
     public function getLastError(): array {
         return array($this->db->errno, $this->db->sqlstate, $this->db->error);
